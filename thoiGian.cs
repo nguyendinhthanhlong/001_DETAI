41caebb3c993e6b7ffc9c031804cdc38b153614d
nguyendinhthanhlong <67327399+nguyendinhthanhlong@users.noreply.github.com>
2020-12-31 16:35:51 +0700

    4.34

---
+++ thoiGian.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace _001_DETAI
+{
+    public class thoiGian
+    {
+        public Trua trua { get; set; }
+        public Sang sang { get; set; }
+        public Toi toi { get; set; }
+        public thoiGian()
+        {
+           
+        }
+        public thoiGian(params int[] thamso)
+        {
+            this.sang.flag = thamso[0];
+            this.trua.flag = thamso[1];
+            this.toi.flag = thamso[2];
+        }
+    }
+}
